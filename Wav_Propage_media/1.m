9df2f8f87ff863231de83501fd2511dbdfe699dd
RDxR10 <risirajdey@gmail.com>
2020-11-25 23:31:33 +0530

    Update 1.m

--- Wav_Propage_media/1.m
+++ Wav_Propage_media/1.m
@@ -8,15 +8,15 @@ x=0:0.01:3;
 Zo1=zeros(size(x));
 for i=1:1000
   t=i*0.01;
-  Ey=Exm*cos(w*tk*x).*exp(-0.5*x);
-  Hz=Hym*cos(w*tk*x-pi/4).*exp(-0.5*x);
+  Ey=Exm*cos(w*t*k*x).*exp(-0.5*x);
+  Hz=Hym*cos(w*t*k*x-pi/4).*exp(-0.5*x);
   figure(1)
   plot3(x,Ey,Zo1,'b');
   hold on;
   plot3(x,Zo1,Hz,'b');
   grid on;
   axis([0,2,-20,20,-20,20])
-  Xlabel('x axis'); ylabel('electric field'); zlabel('magnetic field');
+  xlabel('x axis'); ylabel('electric field'); zlabel('magnetic field');
   set(gcf,'color','w')
   pause(0.01)
   hold off;
